97746b0905c780d18b4e8e37bc82f2ae7a032fc2
handon-today <zeek@farmslink.org>
2026-05-27 08:23:24 +0000

    fix: sitemap 500 에러 수정, 도메인 handontoday.com 고정

--- articles/sitemaps.py
+++ articles/sitemaps.py
@@ -2,12 +2,10 @@ from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 from .models import Article
 
-SITE_URL = 'https://handontoday.com'
 
 class ArticleSitemap(Sitemap):
     changefreq = "daily"
     priority = 0.8
-    protocol = 'https'
 
     def items(self):
         return Article.objects.filter(publish_status='published').order_by('-published_at')
@@ -19,50 +17,13 @@ class ArticleSitemap(Sitemap):
         slug = obj.slug or 'no-slug'
         return f'/article/{obj.id}-{slug}/'
 
-    def get_urls(self, page=1, site=None, protocol=None):
-        from django.contrib.sitemaps import SitemapNotFound
-        from django.utils.http import http_date
-        urls = []
-        for item in self.paginator.page(page).object_list:
-            loc = f"{SITE_URL}{self.location(item)}"
-            priority = self.get_priority(item)
-            lastmod = self.lastmod(item) if callable(self.lastmod) else self.lastmod
-            if hasattr(lastmod, '__call__'):
-                lastmod = lastmod(item)
-            urls.append({
-                'item': item,
-                'location': loc,
-                'lastmod': lastmod,
-                'changefreq': self.changefreq,
-                'priority': str(priority if priority is not None else ''),
-                'alternates': [],
-                'x_default': None,
-            })
-        return urls
-
 
 class StaticSitemap(Sitemap):
     changefreq = "weekly"
     priority = 0.5
-    protocol = 'https'
 
     def items(self):
         return ['articles:home', 'articles:archive']
 
     def location(self, item):
         return reverse(item)
-
-    def get_urls(self, page=1, site=None, protocol=None):
-        urls = []
-        for item in self.items():
-            loc = f"{SITE_URL}{self.location(item)}"
-            urls.append({
-                'item': item,
-                'location': loc,
-                'lastmod': None,
-                'changefreq': self.changefreq,
-                'priority': str(self.priority),
-                'alternates': [],
-                'x_default': None,
-            })
-        return urls
--- config/urls.py
+++ config/urls.py
@@ -1,27 +1,48 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
+from django.contrib.sites.requests import RequestSite
 from django.http import HttpResponse
 from articles.sitemaps import ArticleSitemap, StaticSitemap
+
+
+class FixedDomainSitemap:
+    """sitemap 뷰에 고정 도메인을 주입하는 래퍼"""
+    domain = 'handontoday.com'
+    name = 'handontoday.com'
+
 
 sitemaps = {
     'articles': ArticleSitemap,
     'static': StaticSitemap,
 }
 
-def robots_txt(request):
-    content = """User-agent: *
-Allow: /
-Disallow: /admin/
-Disallow: /login/
-Disallow: /signup/
 
-Sitemap: https://handontoday.com/sitemap.xml"""
+def sitemap_view(request):
+    """도메인을 handontoday.com으로 고정한 sitemap 뷰"""
+    request.META['HTTP_HOST'] = 'handontoday.com'
+    request.META['SERVER_NAME'] = 'handontoday.com'
+    request.META['SERVER_PORT'] = '443'
+    request.META['wsgi.url_scheme'] = 'https'
+    return sitemap(request, sitemaps=sitemaps, sitemap_url_name='sitemap')
+
+
+def robots_txt(request):
+    content = (
+        "User-agent: *\n"
+        "Allow: /\n"
+        "Disallow: /admin/\n"
+        "Disallow: /login/\n"
+        "Disallow: /signup/\n"
+        "\n"
+        "Sitemap: https://handontoday.com/sitemap.xml"
+    )
     return HttpResponse(content, content_type='text/plain')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
+    path('sitemap.xml', sitemap_view, name='sitemap'),
     path('robots.txt', robots_txt),
     path('', include('articles.urls')),
 ]
